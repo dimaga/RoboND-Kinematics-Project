c9ae0c8feaa3f3a7dde34a8649491dc5b63fd4e1
Dmitry Gavrilenko <dmitry.gavrilenko@icloud.com>
2018-06-03 10:42:19 -0700

    Tune up test code

--- IK_debug.py
+++ IK_debug.py
@@ -3,16 +3,6 @@ import numpy as np
 import unittest
 
 
-EE_2_SIX = Matrix([
-    [0.0, 0.0, 1.0, 0.0],
-    [0.0, -1.0, 0.0, 0.0],
-    [1.0, 0.0, 0.0, 0.2305],
-    [0.0, 0.0, 0.0, 1.0],
-])
-
-SIX_2_EE = EE_2_SIX.inv()
-
-
 def dot(a, b):
     """Extend np.dot() to support simpy.Matrix types. This is to avoid confusion with *-operator, which does different
     when applied to different types"""
@@ -41,6 +31,16 @@ def quat_2_rotation(q):
     ])
 
     return result
+
+
+EE_2_SIX = Matrix([
+    [0.0, 0.0, 1.0, 0.0],
+    [0.0, -1.0, 0.0, 0.0],
+    [1.0, 0.0, 0.0, 0.2305],
+    [0.0, 0.0, 0.0, 1.0],
+])
+
+SIX_2_EE = EE_2_SIX.inv()
 
 
 def get_ee_2_wc(joints):
@@ -111,54 +111,57 @@ def get_wc_position(ee_position, ee_rotation):
 class TestIkMethods(unittest.TestCase):
 
     def test_1(self):
+
         self.__test_kinematics(
-            ee_position=[2.16135, -1.42635, 1.55109],
-            ee_quaternion=[0.708611, 0.186356, -0.157931, 0.661967],
-            wc_position=[1.89451, -1.44302, 1.69366],
-            joints=[-0.65, 0.45, -0.36, 0.95, 0.79, 0.49]
+            expected_ee_position=[2.16135, -1.42635, 1.55109],
+            expected_ee_quaternion=[0.708611, 0.186356, -0.157931, 0.661967],
+            expected_wc_position=[1.89451, -1.44302, 1.69366],
+            expected_joints=[-0.65, 0.45, -0.36, 0.95, 0.79, 0.49]
         )
 
 
     def test_2(self):
+
         self.__test_kinematics(
-            ee_position=[-0.56754, 0.93663, 3.0038],
-            ee_quaternion=[0.62073, 0.48318, 0.38759, 0.480629],
-            wc_position=[-0.638, 0.64198, 2.9988],
-            joints=[-0.79, -0.11, -2.33, 1.94, 1.14, -3.68]
+            expected_ee_position=[-0.56754, 0.93663, 3.0038],
+            expected_ee_quaternion=[0.62073, 0.48318, 0.38759, 0.480629],
+            expected_wc_position=[-0.638, 0.64198, 2.9988],
+            expected_joints=[-0.79, -0.11, -2.33, 1.94, 1.14, -3.68]
         )
 
 
     def test_3(self):
+
         self.__test_kinematics(
-            ee_position=[-1.3863, 0.02074, 0.90986],
-            ee_quaternion=[0.01735, -0.2179, 0.9025, 0.371016],
-            wc_position=[-1.1669, -0.17989, 0.85137],
-            joints=[-2.99, -0.12, 0.94, 4.06, 1.29, -4.12]
+            expected_ee_position=[-1.3863, 0.02074, 0.90986],
+            expected_ee_quaternion=[0.01735, -0.2179, 0.9025, 0.371016],
+            expected_wc_position=[-1.1669, -0.17989, 0.85137],
+            expected_joints=[-2.99, -0.12, 0.94, 4.06, 1.29, -4.12]
         )
 
 
-    def __test_kinematics(self, ee_position, ee_quaternion, wc_position, joints):
+    def __test_kinematics(self, expected_ee_position, expected_ee_quaternion, expected_wc_position, expected_joints):
 
         wc_2_base_array = np.array(WC_2_BASE.evalf(subs={
-            JOINTS[0]: joints[0],
-            JOINTS[1]: joints[1],
-            JOINTS[2]: joints[2]})).astype(np.float64)
+            JOINTS[0]: expected_joints[0],
+            JOINTS[1]: expected_joints[1],
+            JOINTS[2]: expected_joints[2]})).astype(np.float64)
 
         ee_2_wc_array = np.array(EE_2_WC.evalf(subs={
-            JOINTS[3]: joints[3],
-            JOINTS[4]: joints[4],
-            JOINTS[5]: joints[5]})).astype(np.float64)
+            JOINTS[3]: expected_joints[3],
+            JOINTS[4]: expected_joints[4],
+            JOINTS[5]: expected_joints[5]})).astype(np.float64)
 
         ee_2_base_array = wc_2_base_array.dot(ee_2_wc_array)
-        ee_rotation = quat_2_rotation(ee_quaternion)
+        expected_ee_rotation = quat_2_rotation(expected_ee_quaternion)
 
-        np.testing.assert_almost_equal(wc_position, wc_2_base_array[:3, 3], decimal=1)
-        np.testing.assert_almost_equal(ee_position, ee_2_base_array[:3, 3], decimal=1)
-        np.testing.assert_almost_equal(ee_rotation, ee_2_base_array[:3, :3], decimal=1)
+        np.testing.assert_almost_equal(expected_wc_position, wc_2_base_array[:3, 3], decimal=1)
+        np.testing.assert_almost_equal(expected_ee_position, ee_2_base_array[:3, 3], decimal=1)
+        np.testing.assert_almost_equal(expected_ee_rotation, ee_2_base_array[:3, :3], decimal=1)
 
         np.testing.assert_almost_equal(
-            wc_position,
-            get_wc_position(ee_position, ee_rotation),
+            expected_wc_position,
+            get_wc_position(expected_ee_position, expected_ee_rotation),
             decimal=1)
 
 

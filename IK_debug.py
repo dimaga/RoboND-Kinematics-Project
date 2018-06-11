9a917628f7dfec6b075ce8f77f08d13de13dd037
Dmitry Gavrilenko <dmitry.gavrilenko@icloud.com>
2018-06-10 23:47:53 -0700

    Implement fast performance branch in IK_debug.py

--- IK_debug.py
+++ IK_debug.py
@@ -1,8 +1,17 @@
-from sympy import sin, cos, Matrix, pi, symbols, simplify
 from itertools import izip
 import numpy as np
 import math
 import unittest
+
+# Replaces slow sympy with faster numpy to achieve real-time performance
+FAST_PERFORMANCE = True
+
+if not FAST_PERFORMANCE:
+    from sympy import sin, cos, Matrix, pi, symbols, simplify
+else:
+    pi = np.pi
+    cos = math.cos
+    sin = math.sin
 
 
 GRAD_2_RAD = np.pi / 180.0
@@ -12,6 +21,9 @@ def dot(a, b):
     """Extend np.dot() to support simpy.Matrix types. This is to avoid confusion with *-operator, which does different
     when applied to different types"""
 
+    if FAST_PERFORMANCE:
+        return np.dot(a, b)
+
     return Matrix(a) * Matrix(b)
 
 
@@ -20,7 +32,7 @@ def quat_2_rotation(q):
 
     qx, qy, qz, qw = q
 
-    result = Matrix([
+    result = np.array([
         [1 - 2 * qy**2 - 2 * qz**2, 2*qx*qy - 2*qz*qw, 2*qx*qz + 2*qy*qw],
         [2*qx*qy + 2*qz*qw, 1 - 2 * qx**2 - 2 * qz**2, 2*qy*qz - 2*qx*qw],
         [2*qx*qz - 2*qy*qw, 2*qy*qz + 2*qx*qw, 1 - 2 * qx**2 - 2 * qy**2]
@@ -71,14 +83,14 @@ def rotation_2_quat(r):
         (r[1, 0] - r[0, 1] ) / s)
 
 
-EE_2_SIX = Matrix([
+EE_2_SIX_ARRAY = np.array([
     [0.0, 0.0, 1.0, 0.0],
     [0.0, -1.0, 0.0, 0.0],
     [1.0, 0.0, 0.0, 0.303],
     [0.0, 0.0, 0.0, 1.0],
 ])
 
-SIX_2_EE = EE_2_SIX.inv()
+SIX_2_EE_ARRAY = np.linalg.inv(EE_2_SIX_ARRAY)
 
 
 def get_dh_transform(alpha, a, d, theta):
@@ -89,32 +101,18 @@ def get_dh_transform(alpha, a, d, theta):
 
     cos_theta = cos(theta)
     sin_theta = sin(theta)
+    
+    if FAST_PERFORMANCE:
+        MatrixType = np.array
+    else:
+        MatrixType = Matrix
 
-    return Matrix([
+    return MatrixType([
         [cos_theta, -sin_theta, 0, a],
         [sin_theta * cos_alpha, cos_theta * cos_alpha, -sin_alpha, -sin_alpha * d],
         [sin_theta * sin_alpha, cos_theta * sin_alpha, cos_alpha, cos_alpha * d],
         [0.0, 0.0, 0.0, 1.0],
     ])
-
-
-JOINTS = symbols('JOINTS0:6')
-
-
-SIX_2_FIVE = get_dh_transform(-pi / 2, 0.0, 0.0, JOINTS[5])
-FIVE_2_FOUR = get_dh_transform(pi / 2, 0.0, 0.0, JOINTS[4])
-FOUR_2_WC = get_dh_transform(0.0, 0.0, 0.0, JOINTS[3])
-
-
-def get_ee_2_wc():
-    """Returns transformation from End Effect to Wrist Center reference frames given joints values"""
-
-    result = dot(SIX_2_FIVE, EE_2_SIX)
-    result = dot(FIVE_2_FOUR, result)
-    result = dot(FOUR_2_WC, result)
-    result = simplify(result)
-
-    return result
 
 
 WC_2_THREE_A = 0.054
@@ -124,43 +122,118 @@ WC_2_THREE_LENGTH = math.sqrt(WC_2_THREE_A**2 + WC_2_THREE_D**2)
 WC_2_THREE_CONST = get_dh_transform(pi / 2, WC_2_THREE_A, WC_2_THREE_D, pi)
 
 THREE_2_TWO_A = 1.25
-THREE_2_TWO = get_dh_transform(0.0, THREE_2_TWO_A, 0.0, pi + JOINTS[2])
 
-TWO_2_ONE_VAR = get_dh_transform(0.0, 0.0, 0.0, JOINTS[1])
 TWO_2_ONE_CONST = get_dh_transform(-pi / 2, 0.35, 0.0, -pi / 2)
-ONE_2_TWO_CONST = simplify(TWO_2_ONE_CONST.inv())
-ONE_2_ZERO = get_dh_transform(0.0, 0.0, 0.75, JOINTS[0])
-ZERO_2_ONE = simplify(ONE_2_ZERO.inv())
+
+if FAST_PERFORMANCE:
+    ONE_2_TWO_CONST = np.linalg.inv(TWO_2_ONE_CONST)
+else:
+    ONE_2_TWO_CONST = TWO_2_ONE_CONST.inv()
+
+    JOINTS = symbols('JOINTS0:6')
+
+    ONE_2_ZERO = get_dh_transform(0.0, 0.0, 0.75, JOINTS[0])
+    ZERO_2_ONE = simplify(ONE_2_ZERO.inv())
+
+    TWO_2_ONE_VAR = get_dh_transform(0.0, 0.0, 0.0, JOINTS[1])
+    THREE_2_TWO = get_dh_transform(0.0, THREE_2_TWO_A, 0.0, pi + JOINTS[2])
+
+    FOUR_2_WC = get_dh_transform(0.0, 0.0, 0.0, JOINTS[3])
+    FIVE_2_FOUR = get_dh_transform(pi / 2, 0.0, 0.0, JOINTS[4])
+    SIX_2_FIVE = get_dh_transform(-pi / 2, 0.0, 0.0, JOINTS[5])
 
 
-def get_wc_2_base():
-    """Returns transformation from Wrist Center to Base (or World) reference frames given joints values"""
+    def get_wc_2_base():
+        """Returns transformation formula from Wrist Center to Base (or World) reference frames given joints symbols"""
 
-    result = dot(THREE_2_TWO, WC_2_THREE_CONST)
-    result = dot(TWO_2_ONE_VAR, result)
-    result = dot(TWO_2_ONE_CONST, result)
-    result = dot(ONE_2_ZERO, result)
-    result = simplify(result)
+        result = dot(THREE_2_TWO, WC_2_THREE_CONST)
+        result = dot(TWO_2_ONE_VAR, result)
+        result = dot(TWO_2_ONE_CONST, result)
+        result = dot(ONE_2_ZERO, result)
+        result = simplify(result)
 
-    return result
-
-
-WC_2_BASE = get_wc_2_base()
-EE_2_WC = get_ee_2_wc()
+        return result
 
 
-def get_full_transform(joints):
-    wc_2_base_array = np.array(WC_2_BASE.evalf(subs=joints)).astype(np.float64)
-    ee_2_wc_array = np.array(EE_2_WC.evalf(subs=joints)).astype(np.float64)
+    def get_ee_2_wc():
+        """Returns transformation formula from End Effect to Wrist Center reference frames given joints symbols"""
+
+        result = dot(SIX_2_FIVE, EE_2_SIX_ARRAY)
+        result = dot(FIVE_2_FOUR, result)
+        result = dot(FOUR_2_WC, result)
+        result = simplify(result)
+
+        return result
+
+
+    WC_2_BASE = get_wc_2_base()
+    EE_2_WC = get_ee_2_wc()
+
+
+def get_wc_2_base_array(joints):
+    """Returns transformation values from Wrist Center to Base (or World) reference frames given joints values"""
+    if FAST_PERFORMANCE:
+        s = map(sin, joints)
+        c = map(cos, joints)
+        
+        j1_plus_2 = joints[1] + joints[2]
+        c_j1_plus_2 = cos(j1_plus_2)
+        s_j1_plus_2 = sin(j1_plus_2)
+        
+        # Precalculated by sympy
+        return np.array([
+            [s_j1_plus_2 * c[0], s[0], c[0] * c_j1_plus_2,
+             (1.25 * s[1] - 0.054 * s_j1_plus_2 + 1.5 * c_j1_plus_2 + 0.35) * c[0]],
+            [s[0] * s_j1_plus_2, -c[0], s[0] * c_j1_plus_2,
+             (1.25 * s[1] - 0.054 * s_j1_plus_2 + 1.5 * c_j1_plus_2 + 0.35) * s[0]],
+            [c_j1_plus_2, 0, -s_j1_plus_2,
+             -1.5 * s_j1_plus_2 + 1.25 * c[1] - 0.054 * c_j1_plus_2 + 0.75],
+            [0, 0, 0, 1.0]])
+
+    named_joints = {name: v for name, v in izip(JOINTS, joints)}
+    return np.array(WC_2_BASE.evalf(subs=named_joints)).astype(np.float64)
+
+
+def get_ee_2_wc_array(joints):
+    """Returns transformation values from End Effect to Wrist Center reference frames given joints values"""
+    if FAST_PERFORMANCE:
+        s = map(sin, joints)
+        c = map(cos, joints)
+
+        # Precalculated by sympy
+        return np.array([
+            [-s[4] * c[3], s[3] * c[5] + s[5] * c[3] * c[4], -s[3] * s[5] + c[3] * c[4] * c[5], -0.303 * s[4] * c[3]],
+            [-s[3] * s[4], s[3] * s[5] * c[4] - c[3] * c[5], s[3] * c[4] * c[5] + s[5] * c[3], -0.303 * s[3] * s[4]],
+            [c[4], s[4] * s[5], s[4] * c[5], 0.303 * c[4]],
+            [0, 0, 0, 1.0]])
+
+
+    named_joints = {name: v for name, v in izip(JOINTS, joints)}
+    return np.array(EE_2_WC.evalf(subs=named_joints)).astype(np.float64)
+
+
+def get_full_transform_array(joints):
+    wc_2_base_array = get_wc_2_base_array(joints)
+    ee_2_wc_array = get_ee_2_wc_array(joints)
     return wc_2_base_array.dot(ee_2_wc_array)
+
+
+def get_zero_2_one_array(joint0):
+    if FAST_PERFORMANCE:
+
+        # Precalculated by sympy
+        return np.array([
+            [cos(joint0), sin(joint0), 0, 0],
+            [-sin(joint0), cos(joint0), 0, 0],
+            [0, 0, 1, -0.75],
+            [0, 0, 0, 1.0]])
+    
+    return np.array(ZERO_2_ONE.evalf(subs={JOINTS[0]: joint0})).astype(np.float64)
 
 
 def get_wc_position(ee_position, ee_rotation):
     """Restores Wrist Center world position given desired End Effector position and rotation passed as 3x3 rotation
     matrix"""
-
-    six_2_ee_array = np.array(SIX_2_EE).astype(np.float64)
-
     ee_rotation_array = np.array(ee_rotation).astype(np.float64).reshape(3, 3)
     ee_position_array = np.array(ee_position).astype(np.float64).reshape(3, 1)
 
@@ -168,7 +241,7 @@ def get_wc_position(ee_position, ee_rotation):
         np.hstack([ee_rotation_array, ee_position_array]),
         [0.0, 0.0, 0.0, 1.0]])
 
-    six_2_zero_array = ee_2_zero_array.dot(six_2_ee_array)
+    six_2_zero_array = ee_2_zero_array.dot(SIX_2_EE_ARRAY)
     return six_2_zero_array[:3, 3]
 
 
@@ -178,8 +251,8 @@ def restore_wc_joint_0(wc_position):
     n_cos_a = wc_position[0]
     n_sin_a = wc_position[1]
 
-    yield { JOINTS[0] : math.atan2(n_sin_a, n_cos_a) }
-    yield { JOINTS[0] : math.atan2(-n_sin_a, -n_cos_a) }
+    yield math.atan2(n_sin_a, n_cos_a)
+    yield math.atan2(-n_sin_a, -n_cos_a)
 
 
 def restore_wc_joints_0_3(wc_position):
@@ -187,7 +260,7 @@ def restore_wc_joints_0_3(wc_position):
 
     for wc_joint_0 in restore_wc_joint_0(wc_position):
 
-        wc_position_in_one = dot(ZERO_2_ONE.evalf(subs=wc_joint_0), [
+        wc_position_in_one = dot(get_zero_2_one_array(wc_joint_0), [
             wc_position[0],
             wc_position[1],
             wc_position[2],
@@ -215,19 +288,13 @@ def restore_wc_joints_0_3(wc_position):
         default_b_angle = math.atan2(WC_2_THREE_D, WC_2_THREE_A)
         wc_angle = math.atan2(wc_position_in_two[0], wc_position_in_two[1])
 
-        wc_joints_0_3 = {JOINTS[1] : np.pi/2 - a_angle - wc_angle}
-        wc_joints_0_3.update(wc_joint_0)
-        wc_joints_0_3.update({JOINTS[2] : default_b_angle - b_angle})
-        yield wc_joints_0_3
-
-        wc_joints_0_3 = {JOINTS[1] : np.pi/2 - wc_angle + a_angle}
-        wc_joints_0_3.update(wc_joint_0)
-        wc_joints_0_3.update({JOINTS[2]: default_b_angle + b_angle})
-        yield wc_joints_0_3
+        yield [wc_joint_0, np.pi/2 - a_angle - wc_angle, default_b_angle - b_angle]
+        yield [wc_joint_0, np.pi/2 - wc_angle + a_angle, default_b_angle + b_angle]
 
 
 def restore_ee_joints_3_6(wc_2_base_rot, ee_2_base_rot, closest_joints):
-    """Restores last three joints using rotation matrix to euler transformation"""
+    """Restores last three joints using rotation matrix to euler transformation. In case of Gimble Lock case, generate
+    2 hypotheses with closest_joints[3] and closest_joints[5] values"""
 
     ee_2_wc_rot = dot(wc_2_base_rot.T, ee_2_base_rot)
 
@@ -246,7 +313,7 @@ def restore_ee_joints_3_6(wc_2_base_rot, ee_2_base_rot, closest_joints):
             cos_j5 = ee_2_wc_rot[2, 2] / sin_j4
             j5 = math.atan2(sin_j5, cos_j5)
 
-            yield {JOINTS[3]: j3, JOINTS[4]: j4, JOINTS[5]: j5}
+            yield [j3, j4, j5]
     else:
         # Gimble Lock case
         j4 = possible_j4
@@ -255,11 +322,13 @@ def restore_ee_joints_3_6(wc_2_base_rot, ee_2_base_rot, closest_joints):
         cos_j3_plus_5 = -ee_2_wc_rot[1, 1]
         j3_plus_5 = math.atan2(sin_j3_plus_5, cos_j3_plus_5)
 
-        yield {JOINTS[3]: j3_plus_5 - closest_joints[5], JOINTS[4]: j4, JOINTS[5]: closest_joints[5]}
-        yield {JOINTS[3]: closest_joints[3], JOINTS[4]: j4, JOINTS[5]: j3_plus_5 - closest_joints[3]}
+        yield [j3_plus_5 - closest_joints[5], j4, closest_joints[5]]
+        yield [closest_joints[3], j4, j3_plus_5 - closest_joints[3]]
 
 
 def normPi(angle):
+    """Warps the angle value to be in the range of [-np.pi, np.pi)"""
+
     if angle > np.pi or angle <= -np.pi:
         angle = angle % (2 * np.pi)
 
@@ -272,37 +341,40 @@ def normPi(angle):
 
 
 def restore_aa_all_joints(ee_position, ee_quaternion, closest_joints):
+    """Returns iterator of possible (error, joints) configuration. In case of ambiguities, joints are selected
+    to be close to closest_joints"""
+
     ee_quaternion_array = np.array(ee_quaternion)
     ee_rotation = quat_2_rotation(ee_quaternion_array)
 
     wc_position = get_wc_position(ee_position, ee_rotation)
 
     for joints_0_3 in restore_wc_joints_0_3(wc_position):
-        joints_0_3[JOINTS[0]] = normPi(joints_0_3[JOINTS[0]])
+        joints_0_3[0] = normPi(joints_0_3[0])
 
-        joints_0_3[JOINTS[1]] = normPi(joints_0_3[JOINTS[1]])
-        joints_0_3[JOINTS[1]] = max(joints_0_3[JOINTS[1]], -45 * GRAD_2_RAD)
-        joints_0_3[JOINTS[1]] = min(joints_0_3[JOINTS[1]], 85 * GRAD_2_RAD)
+        joints_0_3[1] = normPi(joints_0_3[1])
+        joints_0_3[1] = max(joints_0_3[1], -45 * GRAD_2_RAD)
+        joints_0_3[1] = min(joints_0_3[1], 85 * GRAD_2_RAD)
 
-        joints_0_3[JOINTS[2]] = normPi(joints_0_3[JOINTS[2]] + 90 * GRAD_2_RAD)
-        joints_0_3[JOINTS[2]] = max(joints_0_3[JOINTS[2]], (-210 + 90) * GRAD_2_RAD)
-        joints_0_3[JOINTS[2]] = min(joints_0_3[JOINTS[2]], (65 + 90) * GRAD_2_RAD)
-        joints_0_3[JOINTS[2]] = normPi(joints_0_3[JOINTS[2]] - 90 * GRAD_2_RAD)
+        joints_0_3[2] = normPi(joints_0_3[2] + 90 * GRAD_2_RAD)
+        joints_0_3[2] = max(joints_0_3[2], (-210 + 90) * GRAD_2_RAD)
+        joints_0_3[2] = min(joints_0_3[2], (65 + 90) * GRAD_2_RAD)
+        joints_0_3[2] = normPi(joints_0_3[2] - 90 * GRAD_2_RAD)
 
-        wc_2_base_array = np.array(WC_2_BASE.evalf(subs=joints_0_3)).astype(np.float64)
+        wc_2_base_array = get_wc_2_base_array(joints_0_3)
 
-        for joints_all in restore_ee_joints_3_6(wc_2_base_array[:3, :3], ee_rotation, closest_joints):
-            joints_all.update(joints_0_3)
+        for joints_3_6 in restore_ee_joints_3_6(wc_2_base_array[:3, :3], ee_rotation, closest_joints):
+            joints_all = list(joints_0_3) + list(joints_3_6)
 
-            joints_all[JOINTS[3]] = normPi(joints_all[JOINTS[3]])
+            joints_all[3] = normPi(joints_all[3])
 
-            joints_all[JOINTS[4]] = normPi(joints_all[JOINTS[4]])
-            joints_all[JOINTS[4]] = max(joints_all[JOINTS[4]], -125 * GRAD_2_RAD)
-            joints_all[JOINTS[4]] = min(joints_all[JOINTS[4]], 125 * GRAD_2_RAD)
+            joints_all[4] = normPi(joints_all[4])
+            joints_all[4] = max(joints_all[4], -125 * GRAD_2_RAD)
+            joints_all[4] = min(joints_all[4], 125 * GRAD_2_RAD)
 
-            joints_all[JOINTS[5]] = normPi(joints_all[JOINTS[5]])
+            joints_all[5] = normPi(joints_all[5])
 
-            given_ee_2_base_array = get_full_transform(joints_all)
+            given_ee_2_base_array = get_full_transform_array(joints_all)
             given_ee_quaternion_array = np.array(rotation_2_quat(given_ee_2_base_array[:3, :3]))
 
             error_t = np.sum((given_ee_2_base_array[:3, 3] - ee_position)**2)
@@ -343,9 +415,7 @@ class TestGeneric(unittest.TestCase):
         expected_wc_position,
         expected_joints):
 
-        wc_2_base_array = np.array(WC_2_BASE.evalf(subs={
-            JOINTS[i] : expected_joints[i] for i in xrange(3)})).astype(np.float64)
-
+        wc_2_base_array = get_wc_2_base_array(expected_joints)
         np.testing.assert_almost_equal(expected_wc_position, wc_2_base_array[:3, 3], decimal=2)
 
 
@@ -356,9 +426,7 @@ class TestGeneric(unittest.TestCase):
         expected_wc_position,
         expected_joints):
 
-        named_joints = {name: v for name, v in izip(JOINTS, expected_joints)}
-
-        ee_2_base_array = get_full_transform(named_joints)
+        ee_2_base_array = get_full_transform_array(expected_joints)
         np.testing.assert_almost_equal(expected_ee_position, ee_2_base_array[:3, 3], decimal=2)
 
         expected_ee_rotation = quat_2_rotation(expected_ee_quaternion)
@@ -385,7 +453,7 @@ class TestGeneric(unittest.TestCase):
         expected_joints):
 
         for joints_0_3 in restore_wc_joints_0_3(expected_wc_position):
-            wc_2_base_array = np.array(WC_2_BASE.evalf(subs=joints_0_3)).astype(np.float64)
+            wc_2_base_array = get_wc_2_base_array(joints_0_3)
             np.testing.assert_almost_equal(expected_wc_position, wc_2_base_array[:3, 3], decimal=2)
 
 
@@ -401,12 +469,13 @@ class TestGeneric(unittest.TestCase):
         num_solutions = 0
 
         for joints_0_3 in restore_wc_joints_0_3(expected_wc_position):
-            wc_2_base_array = np.array(WC_2_BASE.evalf(subs=joints_0_3)).astype(np.float64)
+            wc_2_base_array = get_wc_2_base_array(joints_0_3)
 
-            for joints_all in restore_ee_joints_3_6(wc_2_base_array[:3, :3], expected_ee_rotation, expected_joints):
-                joints_all.update(joints_0_3)
+            for joints_3_6 in restore_ee_joints_3_6(wc_2_base_array[:3, :3], expected_ee_rotation, expected_joints):
+                joints_all = list(joints_0_3) + list(joints_3_6)
 
-                ee_2_base_array = get_full_transform(joints_all)
+                ee_2_base_array = get_full_transform_array(joints_all)
+
                 np.testing.assert_almost_equal(expected_ee_position, ee_2_base_array[:3, 3], decimal=2)
                 np.testing.assert_almost_equal(expected_ee_rotation, ee_2_base_array[:3, :3], decimal=2)
 
@@ -426,8 +495,7 @@ class TestGeneric(unittest.TestCase):
 
         expected_joints = map(normPi, expected_joints)
 
-        for error, named_joints in restore_aa_all_joints(expected_ee_position, expected_ee_quaternion, expected_joints):
-            joints = [named_joints[j] for j in JOINTS]
+        for error, joints in restore_aa_all_joints(expected_ee_position, expected_ee_quaternion, expected_joints):
             error_details += str(error) + ": " + str(joints)
             error_details += ",\n"
 
@@ -540,21 +608,19 @@ test_dataset(
 class TestIk(unittest.TestCase):
 
     def _test(self, joints):
-
-        named_joints = {name: v for name, v in izip(JOINTS, joints)}
-        expected_ee_2_base_array = get_full_transform(named_joints)
+        expected_ee_2_base_array = get_full_transform_array(joints)
 
         num_solutions = 0
 
-        expected_wc_2_base_array = np.array(WC_2_BASE.evalf(subs=named_joints)).astype(np.float64)
+        expected_wc_2_base_array = get_wc_2_base_array(joints)
 
         for joints_0_3 in restore_wc_joints_0_3(expected_wc_2_base_array[:3, 3]):
-            wc_2_base_array = np.array(WC_2_BASE.evalf(subs=joints_0_3)).astype(np.float64)
+            wc_2_base_array = get_wc_2_base_array(joints_0_3)
 
-            for joints_all in restore_ee_joints_3_6(wc_2_base_array[:3, :3], expected_ee_2_base_array[:3, :3], joints):
-                joints_all.update(joints_0_3)
+            for joints_3_6 in restore_ee_joints_3_6(wc_2_base_array[:3, :3], expected_ee_2_base_array[:3, :3], joints):
+                joints_all = list(joints_0_3) + list(joints_3_6)
 
-                ee_2_base_array = get_full_transform(joints_all)
+                ee_2_base_array = get_full_transform_array(joints_all)
                 np.testing.assert_almost_equal(expected_ee_2_base_array, ee_2_base_array, decimal=2)
 
                 num_solutions += 1
